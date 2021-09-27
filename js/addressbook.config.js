3cc8e30a5c991bb3cf90fd3b436af391efa4f91b
Jerry Corum <corum@me.com>
2021-09-27 09:06:19 -0700

    create a config object via class

--- js/addressbook.config.js
+++ js/addressbook.config.js
@@ -1,23 +1,62 @@
-AddressBook.config = function() {
-  //declare module and global variables
-  var protocol, hostname, port, base, apiVersion, contactsPath, contactsUrl, contactUrl;
-
-  // configure these for your env or use Ansible and overwrite this with a template
-  protocol = 'http',
-  hostname = 'localhost',
-  port = '8081',
-  base = 'api',
-  apiVersion = 'v1',
-  contactsPath = 'contacts',
-
-  // urls for calling goaddr API
-  contactsUrl = `http://${ hostname }:${ port }/${ base }/${ apiVersion }/${ contactsPath }`,
-  contactUrl = function(id) {
-    return `http://${ hostname }:${ port }/${ base }/${ apiVersion }/${ contactsPath }/${ id }`
-  };
-
-  return {
-    contactsUrl: contactsUrl,
-    contactUrl: contactUrl
+AddressBook.Config = class Config {
+  constructor(data) {
+    this.protocol = data.protocol;
+    this.hostname = data.hostname;
+    this.port = data.port;
   }
-}();
+  contactUrl = function(id) {
+    return `${ this.protocol }://${ this.hostname }:${ this.port }/api/v1/contacts/${id}`
+  }
+  contactsUrl = function() {
+    return `${ this.protocol }://${ this.hostname }:${ this.port }/api/v1/contacts`
+  }
+}
+
+// I'm leaving this all here until I confirm the above works
+
+// AddressBook.config = function() {
+//   //declare module and global variables
+//   var setConfig, protocol, hostname, port, base, apiVersion, contactsPath, contactsUrl, contactUrl, getConfig;
+
+//   // Load config file data into config object for app use
+//   setConfig = function(data) {
+//     console.log(data);
+//     protocol = data.protocol;
+//     hostname = data.url;
+//     port = data.port;
+//     contactsUrl = `${ protocol }://${ hostname }:${ port }/${ base }/${ apiVersion }/${ contactsPath }`
+//     return true
+//   }
+
+//   base = 'api',
+//   apiVersion = 'v1',
+//   contactsPath = 'contacts',
+
+//   // urls for calling goaddr API
+//   // contactsUrl = function() {
+//   //   return `${ protocol }://${ hostname }:${ port }/${ base }/${ apiVersion }/${ contactsPath }`
+//   // }
+//   contactUrl = function(id) {
+//     return `${ protocol }//${ hostname }:${ port }/${ base }/${ apiVersion }/${ contactsPath }/${ id }`
+//   };
+
+//   getConfig = function() {
+//     return {
+//       protocol: protocol,
+//       hostname: hostname,
+//       port: port,
+//       base: base,
+//       apiVersion: apiVersion,
+//       contactsPath: contactsPath,
+//       contactUrl: contactUrl,
+//       contactsUrl: contactsUrl
+//     }
+//   }
+
+//   return {
+//     contactsUrl: contactsUrl,
+//     contactUrl: contactUrl,
+//     setConfig: setConfig,
+//     getConfig: getConfig
+//   }
+// }();
